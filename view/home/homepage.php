29ab90775a35a65cd6230c321530639ec6b5be11
Yms Wbrk <mbrkyms@gmail.com>
2023-10-30 15:21:29 +0100

    commit

--- view/home/homepage.php
+++ view/home/homepage.php
@@ -1,8 +1,11 @@
 <h1 class="text-center mt-5">Bienvenue</h1>
 <p class="text-center mt-5">
-    <?php if (isset($_SESSION['token'])  && isset($_GET['token']) && isset($user->token) && $_SESSION['token'] === $user->token && $user->token === $_GET['token']) { ?>
-        <?= isset($_SESSION['email']) ? $_SESSION['email'] : '' ?>
+    <?php
+    if (isset($_SESSION['token'])  && isset($_GET['token']) && isset($user->token)) {
+        if ($_SESSION['token'] === $user->token && $user->token === $_GET['token']) { ?>
+            <?= isset($_SESSION['email']) ? $_SESSION['email'] : '' ?>
     <?php } else {
-        header('location:index.php?logout');
+            header('location:index.php?logout');
+        }
     } ?>
 </p>
