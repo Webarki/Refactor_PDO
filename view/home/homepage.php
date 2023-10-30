d46573efca61a4d8e29021a3113b978a1a3d5b7e
Yms Wbrk <mbrkyms@gmail.com>
2023-10-30 15:08:38 +0100

    commit

--- view/home/homepage.php
+++ view/home/homepage.php
@@ -1,10 +1,7 @@
 <h1 class="text-center mt-5">Bienvenue</h1>
 <p class="text-center mt-5">
-    <?php if ($_SESSION['token'] === $user->token && $user->token === $_GET['token']) {
-        var_dump($_SESSION["token"]);
-        var_dump($user->token);
-        var_dump($_GET["token"]);  ?>
-        <?= $_SESSION['email'] ? $_SESSION['email'] : '' ?>
+    <?php if (isset($_SESSION['token'])  && isset($_GET['token']) && isset($user->token) && $_SESSION['token'] === $user->token && $user->token === $_GET['token']) { ?>
+        <?= isset($_SESSION['email']) ? $_SESSION['email'] : '' ?>
     <?php } else {
         header('location:index.php?logout');
     } ?>
